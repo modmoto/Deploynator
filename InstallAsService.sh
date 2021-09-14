fd2285cac27dca9c4d4a1d36eb551b9717c58930
Simon Heiss <simonheiss87@gmail.com>
2021-09-14 20:55:27 +0200

    asd

--- InstallAsService.sh
+++ InstallAsService.sh
@@ -2,8 +2,9 @@
 sudo mkdir /srv/Deploynator
 sudo systemctl stop Deploynator
 sudo chown pi /srv/Deploynator
-dotnet publish -c Release -o /srv/Deploynator
+dotnet publish -c Release -r linux-arm  --self-contained=true -p:PublishSingleFile=true -p:GenerateRuntimeConfigurationFiles=true -o /srv/Deploynator
 chmod +x /srv/Deploynator/Deploynator
 sudo cp Deploynator.service /etc/systemd/system/Deploynator.service
 sudo systemctl daemon-reload
 sudo systemctl start Deploynator
+sudo systemctl enable Deploynator
