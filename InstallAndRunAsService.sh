6f131218e19283029079bc2a2ea32168f031b62a
Simon Heiss <simonheiss87@gmail.com>
2021-09-14 19:43:15 +0200

    asd

--- InstallAndRunAsService.sh
+++ InstallAndRunAsService.sh
@@ -1,15 +1,10 @@
 ﻿#!/bin/bash
-
 git pull
-
 sudo mkdir /srv/Deploynator
 sudo chown pi /srv/Deploynator
-
 dotnet publish -c Release -o /srv/Deploynator
 chmod +x /srv/Deploynator/Deploynator
-
 sudo systemctl stop Deploynator
 sudo cp Deploynator.service /etc/systemd/system/Deploynator.service
 sudo systemctl daemon-reload
 sudo systemctl start Deploynator
-
