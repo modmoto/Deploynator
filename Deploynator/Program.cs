43839515cb8ed56b07af781a290d15feeb42373c
Simon Heiss <simonheiss87@gmail.com>
2021-09-16 15:13:37 +0200

    who cares

--- Deploynator/Program.cs
+++ Deploynator/Program.cs
@@ -12,6 +12,7 @@ namespace Deploynator
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSystemd()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
