0b2309c1f58f3d4d7dd10ce6f911285b486c2957
Simon Heiss <simonheiss87@gmail.com>
2021-09-17 00:08:20 +0200

    lines

--- Deploynator/LcdScreen.cs
+++ Deploynator/LcdScreen.cs
@@ -50,7 +50,7 @@ namespace Deploynator
                 }
                 else
                 {
-                    line += $" {word}";
+                    line += $"{word} ";
                 }
 
             }
