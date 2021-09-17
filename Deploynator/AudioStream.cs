ee51cfd26ecb285fd372a31f549baf0ee1e0f26b
Simon Heiss <simonheiss87@gmail.com>
2021-09-17 13:18:10 +0200

    remove b lease

--- Deploynator/AudioStream.cs
+++ Deploynator/AudioStream.cs
@@ -25,7 +25,7 @@ namespace Deploynator
 
             _eventBus.DeselectedDeloyment += (_, args) =>
             {
-                Play($"{(args as SelectReleaseDefinitionArgs)?.ReleaseDefinition.Name} removed from Deployment, bitch please, where the balls at");
+                Play($"{(args as SelectReleaseDefinitionArgs)?.ReleaseDefinition.Name} removed from Deployment, bitch please");
             };
 
             var config = SpeechConfig.FromSubscription("990a253fc3cb487e8f02867fcd3d86c2", "francecentral");
